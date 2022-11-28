ae6a8bfb8f90d8f62bdbc77f98add16585fc4e03
Nataly <naty.sh@mail.ru>
2022-11-28 23:51:56 +0500

    выполнена задача 34

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+int[] CreateMassive(int col)
+{
+    int[] array = new int[col];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+        Console.Write($"{array[i]}  ");
+    }
+    return array;
+}
+
+int AmountEven(int[] massiv)
+{
+    int coant = 0;
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        if (massiv[i] % 2 == 0)
+        {
+            coant++;
+        }
+    }
+    return coant;
+}
+
+
+Console.WriteLine("Введите количество элементов массива");
+int col = Convert.ToInt32(Console.ReadLine());
+int[] massiv = CreateMassive(col);
+Console.WriteLine();
+Console.WriteLine($"Количество четных чисел в массиве={AmountEven(massiv)}");
+
